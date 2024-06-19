bb90b591a57a03749a35b991b9a8969c1174f6b3
Sachin Sethi <sachinsethi2017@gmail.com>
2024-06-19 13:01:03 +0100

    wrote my approach to for the tests

---
+++ Tests/HolidaySearchTests.cs
@@ -0,0 +1,22 @@
+// Load Data
+// Initialise data service
+// Load flights data
+// Load hotels data
+
+// Test Case 1: Manchester to Malaga
+// Initialise holiday search service with data
+// Search from Manchester to Malaga, 1st July 2023, 7 days
+// Verify results are not null and contain results
+// Verify first result has flight ID 2 and hotel ID 9
+
+// Test Case 2: Any London airport to Mallorca
+// Initialise holiday search service with data
+// Search from any London airport to Mallorca, 15th June 2023, 10 days
+// Verify results are not null and contain results
+// Verify first result has flight ID 6 and hotel ID 5
+
+// Test Case 3: Any airport to Gran Canaria
+// Initialise holiday search service with data
+// Search from any airport to Gran Canaria, 10th November 2022, 14 days
+// Verify results are not null and contain results
+// Verify first result has flight ID 7 and hotel ID 6
